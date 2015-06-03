5f5019a595c4921a505214fd1c79fa079ef10a3e
niluje <eric.leibenguth@gmail.com>
2015-06-03 19:04:42 +0200

    Basic skeleton

---
+++ js/tyle.js
@@ -0,0 +1,14 @@
+$(function() {
+  $('.down-icon').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
