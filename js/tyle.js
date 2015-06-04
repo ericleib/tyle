7fd749dbfbb520528b9bae254a5f5e83e4ce2753
niluje <eric.leibenguth@gmail.com>
2015-06-04 18:20:09 +0200

    added 1st slide content

--- js/tyle.js
+++ js/tyle.js
@@ -1,5 +1,8 @@
+
+
+/** Smooth scroll **/
 $(function() {
-  $('.down-icon').click(function() {
+  $('.smooth-scroll').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -11,4 +14,19 @@ $(function() {
       }
     }
   });
+});
+
+
+/** Highlight platform **/
+$(function(){
+  var os = navigator.platform.toLowerCase();
+
+  if(os.includes('linux')){
+    $('#linfav').css('background-color', 'rgb(219, 255, 212)');
+  }else if(os.includes('win')){
+    $('#winfav').css('background-color', 'rgb(219, 255, 212)');
+  }else if(os.includes('mac')){
+    $('#macfav').css('background-color', 'rgb(219, 255, 212)');
+  }
+
 });
