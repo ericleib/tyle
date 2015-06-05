641ddfa26802d7244fe7301c35e3e423419e926f
niluje <eric.leibenguth@gmail.com>
2015-06-05 15:19:35 +0200

    added slide 3-4 and some awesomeness

--- js/tyle.js
+++ js/tyle.js
@@ -31,6 +31,138 @@ $(function(){
 
 });
 
+/** Create 3D wheel **/
+$(document).ready(function(){
+
+  $('#myCarousel')
+  .carousel({interval: false})
+  .on('slide.bs.carousel', function (e) {
+    setTimeout(onWindowResize,0) ;
+  });
+
+
+  var container = $("#wheel3D")[0];
+
+  var camera, scene, renderer, material, mesh, plane;
+
+  var targetRotation = 0;
+  var targetRotationOnMouseDown = 0;
+
+  var mouseX = 0;
+  var mouseXOnMouseDown = 0;
+
+  var windowHalfX = container.clientWidth;
+  var windowHalfY = container.clientHeight;
+
+  init();
+  animate();
+
+  function init() {
+
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera( 70, 1 , 1, 1000 );
+    camera.position.y = 100;
+    camera.position.z = 100;
+
+    scene.add(camera);
+
+    // cylinder
+
+    var geometry = new THREE.CylinderGeometry( 50, 50, 10, 32 );
+
+    material = new THREE.MeshNormalMaterial();
+
+    mesh = new THREE.Mesh( geometry, material );
+    mesh.position.y = 100;
+    scene.add( mesh );
+
+    // Plane
+
+    geometry = new THREE.PlaneBufferGeometry( 200, 200 );
+    geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
+
+    material = new THREE.MeshBasicMaterial( { color: 0xe0e0e0, overdraw: 0.5 } );
+
+    plane = new THREE.Mesh( geometry, material );
+    scene.add( plane );
+
+    renderer = new THREE.CanvasRenderer();
+    renderer.setClearColor( 0xDC493E );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( container.clientWidth, container.clientHeight );
+    container.appendChild( renderer.domElement );
+
+    document.addEventListener( 'mousedown', onDocumentMouseDown, false );
+    document.addEventListener( 'touchstart', onDocumentTouchStart, false );
+    document.addEventListener( 'touchmove', onDocumentTouchMove, false );
+    window.addEventListener( 'resize', onWindowResize, false );
+  }
+
+  function onWindowResize() {
+    windowHalfX = container.clientWidth / 2;
+    windowHalfY = container.height / 2;
+    camera.aspect = container.clientWidth / container.clientHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( container.clientWidth, container.clientHeight );
+  }
+
+
+  function onDocumentMouseDown( event ) {
+    event.preventDefault();
+    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+    document.addEventListener( 'mouseup', onDocumentMouseUp, false );
+    document.addEventListener( 'mouseout', onDocumentMouseOut, false );
+    mouseXOnMouseDown = event.clientX - windowHalfX;
+    targetRotationOnMouseDown = targetRotation;
+  }
+
+  function onDocumentMouseMove( event ) {
+    mouseX = event.clientX - windowHalfX;
+    targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.02;
+  }
+
+  function onDocumentMouseUp( event ) {
+    document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+    document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+    document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+  }
+
+  function onDocumentMouseOut( event ) {
+    document.removeEventListener( 'mousemove', onDocumentMouseMove, false );
+    document.removeEventListener( 'mouseup', onDocumentMouseUp, false );
+    document.removeEventListener( 'mouseout', onDocumentMouseOut, false );
+  }
+
+  function onDocumentTouchStart( event ) {
+    if ( event.touches.length === 1 ) {
+      event.preventDefault();
+      mouseXOnMouseDown = event.touches[ 0 ].pageX - windowHalfX;
+      targetRotationOnMouseDown = targetRotation;
+    }
+  }
+
+  function onDocumentTouchMove( event ) {
+    if ( event.touches.length === 1 ) {
+      event.preventDefault();
+      mouseX = event.touches[ 0 ].pageX - windowHalfX;
+      targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.05;
+    }
+  }
+
+  function animate() {
+    requestAnimationFrame( animate );
+    render();
+  }
+
+  function render() {
+    mesh.rotation.x +=0.01;
+    mesh.rotation.z +=0.02;
+    mesh.rotation.y += ( targetRotation - mesh.rotation.y ) * 0.05;
+    renderer.render( scene, camera );
+  }
+});
+
 
 /** Populate release notes **/
 $(document).ready(function(){
